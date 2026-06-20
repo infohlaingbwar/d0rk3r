622c905e1db81ae4a92a82c77ce053142d1162f9
Hlaing Bwar <infohlaingbwar@gmail.com>
2026-06-20 15:57:28 +0630

    release: v1.0.4 - updated documentation and email
    - Updated all usage examples to python -m d0rk3r
    - Changed email to info@hlaingbwar.com
    - PyPI package updated with latest docs

--- d0rk3r/__init__.py
+++ d0rk3r/__init__.py
@@ -3,7 +3,7 @@ D0RK3R - Shodan IP Scraper with Auto-Proxy Rotation
 Author: Hlaing Bwar (https://github.com/infohlaingbwar)
 """
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "Hlaing Bwar"
 __email__ = "info@hlaingbwar.com"
 __license__ = "MIT"
